2524d2aeea911e0f3e7fd90649731d40d538dbe7
haducacarlo1220 <ralphhaduca@gmail.com>
2024-09-15 10:02:30 +0800

    Initial commit with dataConversion.js

---
+++ dataConversion.js
@@ -0,0 +1,23 @@
+//Convert a string to a number
+let price = "200";
+let num1 = parseInt(price);
+console.log(num1);
+
+let num2 = Number(price);
+console.log(num2);
+
+let num3 = +price;
+console.log(num3);
+
+//Convert a number to a string
+let distance = 150;
+let str1 = distance.toString();
+console.log(str1);
+
+let str2 = String(distance);
+console.log(str2);
+
+//Convert a String to a decimal number
+let weight = "72.5";
+let decimalWeight = parseFloat(weight);
+console.log(decimalWeight);
